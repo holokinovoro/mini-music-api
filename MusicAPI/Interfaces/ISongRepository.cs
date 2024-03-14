b1bde00e6281362d1b135dfc71053c589b2b82ec
Iman Chynybaev <holokinovoro.hab@gmail.com>
2024-03-15 01:41:09 +0300

    Adding SongRepository Iand Interface

---
+++ MusicAPI/Interfaces/ISongRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Identity.Client;
+using MusicAPI.Models;
+
+namespace MusicAPI.Interfaces
+{
+    public interface ISongRepository
+    {
+        ICollection<Song> GetSongs();
+
+        Song GetSong(int songId);
+
+        Artist GetArtistBySong(int songId);
+
+        ICollection<Genre> GetGenreOfSong(int songId);
+
+        bool SongExists(int songId);
+    }
+}
---
+++ MusicAPI/Repositories/SongRepository.cs
@@ -0,0 +1,42 @@
+﻿using MusicAPI.Data;
+using MusicAPI.Interfaces;
+using MusicAPI.Models;
+
+namespace MusicAPI.Repositories
+{
+    public class SongRepository : ISongRepository
+    {
+        private readonly DataContext _context;
+
+        public SongRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Artist GetArtistBySong(int songId)
+        {
+            return _context.Songs.Where(e => e.Id == songId).Select(p => p.Artist).FirstOrDefault();
+        }
+
+        public ICollection<Genre> GetGenreOfSong(int songId)
+        {
+            return _context.Songs.Where(e => e.Id == songId)
+                .SelectMany(s => s.Artist.ArtistGenres.Select(ag => ag.Genre))
+                .Distinct().ToList();
+        }
+
+        public Song GetSong(int songId)
+        {
+            return _context.Songs.Where(e => e.Id == songId).FirstOrDefault();
+        }
+
+        public ICollection<Song> GetSongs()
+        {
+            return _context.Songs.ToList();
+        }
+
+        public bool SongExists(int songId)
+        {
+            return _context.Songs.Any(e => e.Id == songId);
+        }
+    }
+}
